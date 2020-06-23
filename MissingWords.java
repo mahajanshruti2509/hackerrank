e35ac82fa267f081b09414f67400bd10dd07821e
Shruti Mahajan <47750477+mahajanshruti2509@users.noreply.github.com>
2020-06-22 22:10:52 -0500

    Missing Words

--- MissingWords.java
+++ MissingWords.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 class MissingWords
 {
 
@@ -5,28 +7,29 @@ class MissingWords
 
     String[] a = t.split(" ");
     String[] b = s.split(" ");
-    int diff = a.length - b.length;
-    String [] missing = new String[diff];
-    int c = 0;
-    for(int i = 0; i < a.length; i++){
-      int flag = 0;
-      for(int j = 0; j < b.length; j++) {
-        if(a[i].equals(b[j])) {
-          flag = 1;
-          break;
-        }
-      }
-      if(flag == 0) {
-        missing[c++] = a[i];
+    if(a.length < b.length) {
+      return missingWords(s, t);
+    }
+    String[] missing = new String[a.length - b.length];
+
+    HashSet<String> set = new HashSet<>();
+    for(String word : a) {
+      set.add(word);
+    }
+
+    for(String word : b) {
+      if(set.contains(word)) {
+        set.remove(word);
       }
     }
+    set.toArray(missing);
     return missing;
   }
 
   public static void main(String [] args) {
     String t = "I am using hackerrank to improve programming";
     String s = "am hackerrank to improve";
-    String[] str = missingWords(t, s);
+    String[] str = missingWords(s, t);
     for(int i = 0; i <str.length; i++) {
       System.out.println(str[i]);
     }
